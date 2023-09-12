4e2a2b3e60d3d50e1107e708089de448de8c31f6
AbuSayedDev <abusayed285235@gmail.com>
2023-09-12 16:31:53 +0600

    Blog page dynamic

--- index.php
+++ index.php
@@ -1,160 +1,58 @@
 <?php get_header(); ?>
 
 
-    <!-- Breadcumb Area Start Here -->
+<!-- Breadcumb Area Start Here -->
+<section class="breadcumb-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-xxl-12 text-center">
+                <h4><?php single_post_title(); ?></h4>
+                <ul>
+                    <li><a href="<?php echo site_url( ); ?>">home</a></li> / 
+                    <li><?php single_post_title(); ?></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Breadcumb Area End Here -->
 
-    <?php get_template_part('inc/breadcumb'); ?>
-    
-    <!-- Breadcumb Area End Here -->
 
     <section class="blog-page-area pt-100 pb-100">
         <div class="container">
             <div class="row">
-                <div class="col-md-4">
-                    <div class="single-blog">
-                        <img src="<?php echo get_template_directory_uri();?>/assets/images/blog/blog1.jpg" alt="">
-                        <div class="blog-content">
-                            <div class="blog-title">
-                                <h4><a href="">blog title</a></h4>
+
+            <?php 
+
+                while(have_posts()){
+                    the_post();
+
+                    ?>
+
+                    <div class="col-md-4">
+                        <div class="single-blog">
+                            <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
+                            <div class="blog-content">
+                                <div class="blog-title">
+                                    <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+                                </div>
+                                <div class="blog-meta">
+                                    <a href="<?php the_permalink(); ?>"><?php the_date(); ?>\</a>
+                                    <?php the_category(' '); ?>\
+                                    <a href="<?php the_permalink(); ?>"><?php the_author(); ?></a>
+                                </div>
+                                <?php the_content(); ?>
+                                <a href="<?php the_permalink(); ?>" class="box-btn">read more</a>
                             </div>
-                            <div class="blog-meta">
-                                <a href="">5 Jan 2020</a>
-                                <a href="">admin</a>
-                            </div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis magnam a, culpa quis illum aut sint at. Nesciunt tempora officiis labore ducimus laborum saepe dicta?</p>
-                            <a href="" class="box-btn">read more</a>
                         </div>
                     </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="single-blog">
-                        <img src="<?php echo get_template_directory_uri();?>/assets/images/blog/blog2.jpg" alt="">
-                        <div class="blog-content">
-                            <div class="blog-title">
-                                <h4><a href="">blog title</a></h4>
-                            </div>
-                            <div class="blog-meta">
-                                <a href="">5 Jan 2020</a>
-                                <a href="">admin</a>
-                            </div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis magnam a, culpa quis illum aut sint at. Nesciunt tempora officiis labore ducimus laborum saepe dicta?</p>
-                            <a href="" class="box-btn">read more</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="single-blog">
-                        <img src="<?php echo get_template_directory_uri();?>/assets/images/blog/blog3.jpg" alt="">
-                        <div class="blog-content">
-                            <div class="blog-title">
-                                <h4><a href="">blog title</a></h4>
-                            </div>
-                            <div class="blog-meta">
-                                <a href="">5 Jan 2020</a>
-                                <a href="">admin</a>
-                            </div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis magnam a, culpa quis illum aut sint at. Nesciunt tempora officiis labore ducimus laborum saepe dicta?</p>
-                            <a href="" class="box-btn">read more</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="single-blog">
-                        <img src="<?php echo get_template_directory_uri();?>/assets/images/blog/blog3.jpg" alt="">
-                        <div class="blog-content">
-                            <div class="blog-title">
-                                <h4><a href="">blog title</a></h4>
-                            </div>
-                            <div class="blog-meta">
-                                <a href="">5 Jan 2020</a>
-                                <a href="">admin</a>
-                            </div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis magnam a, culpa quis illum aut sint at. Nesciunt tempora officiis labore ducimus laborum saepe dicta?</p>
-                            <a href="" class="box-btn">read more</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="single-blog">
-                        <img src="<?php echo get_template_directory_uri();?>/assets/images/blog/blog3.jpg" alt="">
-                        <div class="blog-content">
-                            <div class="blog-title">
-                                <h4><a href="">blog title</a></h4>
-                            </div>
-                            <div class="blog-meta">
-                                <a href="">5 Jan 2020</a>
-                                <a href="">admin</a>
-                            </div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis magnam a, culpa quis illum aut sint at. Nesciunt tempora officiis labore ducimus laborum saepe dicta?</p>
-                            <a href="" class="box-btn">read more</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="single-blog">
-                        <img src="<?php echo get_template_directory_uri();?>/assets/images/blog/blog3.jpg" alt="">
-                        <div class="blog-content">
-                            <div class="blog-title">
-                                <h4><a href="">blog title</a></h4>
-                            </div>
-                            <div class="blog-meta">
-                                <a href="">5 Jan 2020</a>
-                                <a href="">admin</a>
-                            </div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis magnam a, culpa quis illum aut sint at. Nesciunt tempora officiis labore ducimus laborum saepe dicta?</p>
-                            <a href="" class="box-btn">read more</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="single-blog">
-                        <img src="<?php echo get_template_directory_uri();?>/assets/images/blog/blog3.jpg" alt="">
-                        <div class="blog-content">
-                            <div class="blog-title">
-                                <h4><a href="">blog title</a></h4>
-                            </div>
-                            <div class="blog-meta">
-                                <a href="">5 Jan 2020</a>
-                                <a href="">admin</a>
-                            </div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis magnam a, culpa quis illum aut sint at. Nesciunt tempora officiis labore ducimus laborum saepe dicta?</p>
-                            <a href="" class="box-btn">read more</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="single-blog">
-                        <img src="<?php echo get_template_directory_uri();?>/assets/images/blog/blog3.jpg" alt="">
-                        <div class="blog-content">
-                            <div class="blog-title">
-                                <h4><a href="">blog title</a></h4>
-                            </div>
-                            <div class="blog-meta">
-                                <a href="">5 Jan 2020</a>
-                                <a href="">admin</a>
-                            </div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis magnam a, culpa quis illum aut sint at. Nesciunt tempora officiis labore ducimus laborum saepe dicta?</p>
-                            <a href="" class="box-btn">read more</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="single-blog">
-                        <img src="<?php echo get_template_directory_uri();?>/assets/images/blog/blog3.jpg" alt="">
-                        <div class="blog-content">
-                            <div class="blog-title">
-                                <h4><a href="">blog title</a></h4>
-                            </div>
-                            <div class="blog-meta">
-                                <a href="">5 Jan 2020</a>
-                                <a href="">admin</a>
-                            </div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis magnam a, culpa quis illum aut sint at. Nesciunt tempora officiis labore ducimus laborum saepe dicta?</p>
-                            <a href="" class="box-btn">read more</a>
-                        </div>
-                    </div>
-                </div>
+             <?php   }
+
+                wp_reset_postdata();
+            ?>
+
             </div>
+
             <div class="row">
                 <div class="col-xxl-12">
                     <nav aria-label="Page navigation example">
