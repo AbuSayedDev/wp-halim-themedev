2745fdbe7def2012094d156aa5d223cd8d282ace
AbuSayedDev <abusayed285235@gmail.com>
2023-09-12 15:23:57 +0600

    About page dynamic

--- header.php
+++ header.php
@@ -58,7 +58,7 @@
         <div class="row">
             <div class="col-xxl-12">
                 <nav class="navbar navbar-expand-lg navbar-light">
-                    <a class="navbar-brand" href="index.html">Halim</a>
+                    <a class="navbar-brand" href="<?php echo site_url(); ?>">Halim</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
--- template-about.php
+++ template-about.php
@@ -18,54 +18,53 @@ get_header(); ?>
      <div class="section about-area pt-100 pb-100">
         <div class="container">
             <div class="row section-title align-items-center">
+
+            <?php 
+
+                $about_us_title = get_field('about_us_title', 'option');
+                $about_us_content = get_field('about_us_content', 'option');
+            ?>
+
                 <div class="col-md-6 text-md-end text-sm-center">
-                    <span>who we are?</span>
-                    <h4>about us</h4>
+                    <span><?php echo $about_us_title['subtitle']; ?></span>
+                    <h4><?php echo $about_us_title['title']; ?></h4>
                 </div>
                 <div class="col-md-6">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo vitae dicta, hic sapiente sit perspiciatis modi officiis inventore architecto minima.</p>
+                    <?php echo $about_us_title['description']; ?>
                 </div>
             </div>
+
             <div class="row">
+
                 <div class="col-xl-7 col-lg-6">
                     <div class="about-content">
-                        <h4>welcome to halim</h4>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, ipsa! Officia, nihil accusantium perspiciatis officiis assumenda odio nemo amet cupiditate.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, ipsa! Officia, nihil accusantium perspiciatis officiis assumenda odio nemo amet cupiditate.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, ipsa! Officia, nihil accusantium perspiciatis officiis assumenda odio nemo amet cupiditate.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, ipsa! Officia, nihil accusantium perspiciatis officiis assumenda odio nemo amet cupiditate.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, ipsa! Officia, nihil accusantium perspiciatis officiis assumenda odio nemo amet cupiditate.</p>
-                        <a href="" class="box-btn">read more</a>
+                        <h4><?php echo $about_us_content['title']; ?></h4>
+                        <?php echo $about_us_content['description']; ?>
                     </div>
                 </div>
+                
                 <div class="col-xl-5 col-lg-6 mt-5 mt-lg-0">
-                    <div class="single-about">
-                        <div class="icon">
-                            <i class="fas fa-laptop"></i>
-                        </div>
-                        <div>
-                            <h4>our mission</h4>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam corrupti sunt quibusdam, error amet ex excepturi neque at! Dolor, atque.</p>
-                        </div>
-                    </div>
-                    <div class="single-about">
-                        <div class="icon">
-                            <i class="fas fa-user"></i>
-                        </div>
-                        <div>
-                            <h4>our vission</h4>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam corrupti sunt quibusdam, error amet ex excepturi neque at! Dolor, atque.</p>
-                        </div>
-                    </div>
-                    <div class="single-about">
-                        <div class="icon">
-                            <i class="fas fa-pencil-alt"></i>
-                        </div>
-                        <div>
-                            <h4>our history</h4>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam corrupti sunt quibusdam, error amet ex excepturi neque at! Dolor, atque.</p>
-                        </div>
-                    </div>
+
+                    <?php
+
+                        $about_us_features = get_field('about_us_features', 'option');
+                        
+                        foreach($about_us_features as $about_us_feature){ ?>
+
+                            <div class="single-about">
+                                <div class="icon">
+                                    <i class="<?php echo $about_us_feature['icon']; ?>"></i>
+                                </div>
+                                <div>
+                                    <h4><?php echo $about_us_feature['title']; ?></h4>
+                                    <?php echo $about_us_feature['description']; ?>
+                                </div>
+                            </div>
+
+                     <?php   }
+
+                    ?>
+
                 </div>
             </div>
         </div>
