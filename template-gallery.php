8e199f7b271e7a82f01956c1871c561eb4ba1678
AbuSayedDev <abusayed285235@gmail.com>
2023-09-12 12:58:35 +0600

    Gallery page dynamic

--- template-gallery.php
+++ template-gallery.php
@@ -25,60 +25,43 @@ get_header(); ?>
     <section class="gallery-area pt-100 pb-100">
         <div class="container">
             <div class="row">
-                <div class="col-md-4">
-                    <div class="single-gallery">
-                        <img src="<?php echo get_template_directory_uri();?>/assets/images/gallery/gallery1.jpg" alt="">
-                        <div class="gallery-overlay">
-                            <a href="<?php echo get_template_directory_uri();?>/assets/images/gallery/gallery1.jpg" class="gallery"><i class="fas fa-plus"></i></a>
-                            <h4>item name</h4>
+
+            <?php 
+
+                $args = array(
+                    'post_type'         => 'gallery',
+                    'posts_per_page'    => 10,
+                    'order'             => 'DESC'
+                );
+
+                $query = new WP_Query($args);
+
+                if($query->have_posts()){
+                    while($query->have_posts()){
+                        $query-> the_post();
+
+                    ?>
+                        <div class="col-md-4">
+                            <div class="single-gallery">
+                                <?php the_post_thumbnail( ); ?>
+                                <div class="gallery-overlay">
+                                    <a href="<?php the_post_thumbnail_url(); ?>" class="gallery"><i class="fas fa-plus"></i></a>
+                                    <h4><?php the_title(); ?></h4>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="single-gallery">
-                        <img src="<?php echo get_template_directory_uri();?>/assets/images/gallery/gallery2.jpg" alt="">
-                        <div class="gallery-overlay">
-                            <a href="<?php echo get_template_directory_uri();?>/assets/images/gallery/gallery2.jpg" class="gallery"><i class="fas fa-plus"></i></a>
-                            <h4>item name</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="single-gallery">
-                        <img src="<?php echo get_template_directory_uri();?>/assets/images/gallery/gallery3.jpg" alt="">
-                        <div class="gallery-overlay">
-                            <a href="<?php echo get_template_directory_uri();?>/assets/images/gallery/gallery3.jpg" class="gallery"><i class="fas fa-plus"></i></a>
-                            <h4>item name</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="single-gallery">
-                        <img src="<?php echo get_template_directory_uri();?>/assets/images/gallery/gallery4.jpg" alt="">
-                        <div class="gallery-overlay">
-                            <a href="<?php echo get_template_directory_uri();?>/assets/images/gallery/gallery4.jpg" class="gallery"><i class="fas fa-plus"></i></a>
-                            <h4>item name</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="single-gallery">
-                        <img src="<?php echo get_template_directory_uri();?>/assets/images/gallery/gallery5.jpg" alt="">
-                        <div class="gallery-overlay">
-                            <a href="<?php echo get_template_directory_uri();?>/assets/images/gallery/gallery5.jpg" class="gallery"><i class="fas fa-plus"></i></a>
-                            <h4>item name</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="single-gallery">
-                        <img src="<?php echo get_template_directory_uri();?>/assets/images/gallery/gallery6.jpg" alt="">
-                        <div class="gallery-overlay">
-                            <a href="<?php echo get_template_directory_uri();?>/assets/images/gallery/gallery6.jpg" class="gallery"><i class="fas fa-plus"></i></a>
-                            <h4>item name</h4>
-                        </div>
-                    </div>
-                </div>
+
+              <?php    }
+
+                }else{ ?>
+
+                    <p>No Gallery Post</p>
+
+             <?php }
+
+             wp_reset_postdata();
+
+            ?>
             </div>
         </div>
     </section>
